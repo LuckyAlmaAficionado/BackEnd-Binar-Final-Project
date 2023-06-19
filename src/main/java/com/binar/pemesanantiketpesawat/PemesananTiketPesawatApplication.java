a229c012a496460d974e6d000118ffb1f41a192a
Rigel9802 <luckyrigel9802@gmail.com>
2023-06-19 20:00:56 +0700

    add cors origin

--- src/main/java/com/binar/pemesanantiketpesawat/PemesananTiketPesawatApplication.java
+++ src/main/java/com/binar/pemesanantiketpesawat/PemesananTiketPesawatApplication.java
@@ -3,6 +3,9 @@ package com.binar.pemesanantiketpesawat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan("com.binar.pemesanantiketpesawat.model")
@@ -13,5 +16,18 @@ public class PemesananTiketPesawatApplication {
         SpringApplication.run(PemesananTiketPesawatApplication.class, args);
     }
 
-
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "http://localhost:3001",
+                                "https://binar-backend-final-project-production.up.railway.app",
+                                "*");
+            }
+        };
+    }
 }
