543f88a66173e0cb4c7d85b9dec626799f833162
Rigel9802 <luckyrigel9802@gmail.com>
2023-06-19 21:23:15 +0700

    update

--- src/main/java/com/binar/pemesanantiketpesawat/PemesananTiketPesawatApplication.java
+++ src/main/java/com/binar/pemesanantiketpesawat/PemesananTiketPesawatApplication.java
@@ -3,9 +3,6 @@ package com.binar.pemesanantiketpesawat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan("com.binar.pemesanantiketpesawat.model")
@@ -16,19 +13,4 @@ public class PemesananTiketPesawatApplication {
         SpringApplication.run(PemesananTiketPesawatApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:3000",
-                                "http://localhost:3001",
-                                "https://backend-binar-final-project-production.up.railway.app",
-                                "https://backend-binar-final-project-production.up.railway.app/swagger-ui/index.html#/",
-                                "*");
-            }
-        };
-    }
 }
