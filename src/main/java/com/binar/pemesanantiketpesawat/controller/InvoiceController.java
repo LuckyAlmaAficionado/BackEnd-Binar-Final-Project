1af8fa3f864407cbb9bebc47bd8bb4edcb840d19
Rigel9802 <luckyrigel9802@gmail.com>
2023-06-29 00:24:15 +0700

    update

--- src/main/java/com/binar/pemesanantiketpesawat/controller/InvoiceController.java
+++ src/main/java/com/binar/pemesanantiketpesawat/controller/InvoiceController.java
@@ -2,7 +2,10 @@ package com.binar.pemesanantiketpesawat.controller;
 
 import com.binar.pemesanantiketpesawat.service.InvoiceService;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +21,17 @@ public class InvoiceController {
     @Autowired
     private InvoiceService invoiceService;
 
-    @GetMapping("/{codeBookingRequest}")
-    private void getBookingTicket(HttpServletResponse response, @PathVariable String codeBookingRequest) throws JRException, IOException {
-        System.out.println(codeBookingRequest);
-        invoiceService.searchBookingCodeByCodeBooking(response, codeBookingRequest);
+    @GetMapping("/{bookingCodeRequest}")
+    private ResponseEntity<byte[]> generateInvoice(@PathVariable String bookingCodeRequest) throws JRException, IOException {
+
+        JasperPrint jasperPrint = invoiceService.generateInvoice(bookingCodeRequest);
+        byte[] pdfBytes = JasperExportManager.exportReportToPdf(jasperPrint);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDisposition(ContentDisposition.attachment().filename("invoice.pdf").build());
+
+        return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/binar/pemesanantiketpesawat/service/InvoiceService.java
+++ src/main/java/com/binar/pemesanantiketpesawat/service/InvoiceService.java
@@ -1,13 +1,14 @@
 package com.binar.pemesanantiketpesawat.service;
 
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperPrint;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @Component
 public interface InvoiceService {
-
-    void searchBookingCodeByCodeBooking(HttpServletResponse response, String codeBooking) throws JRException, IOException;
+    JasperPrint generateInvoice(String bookingCodeResponse) throws IOException, JRException;
 }
