cd48b83e8787391487386be650ca1598722fba62
Aditya Prabowo <dityarvp@gmail.com>
2023-07-01 19:33:58 +0700

    Merapikan beberapa line code yang error

--- src/main/java/com/binar/pemesanantiketpesawat/controller/ScheduleController.java
+++ src/main/java/com/binar/pemesanantiketpesawat/controller/ScheduleController.java
@@ -174,8 +174,5 @@ public class ScheduleController {
         Schedule scheduleResponse = scheduleService.updateSchedule(scheduleRequest);
         messageModel.setData(scheduleResponse);
         return ResponseEntity.ok(messageModel);
-    @GetMapping("/duration/{departureTime}/{arrivalTime}")
-    public String getFlightDuration(@PathVariable String departureTime, @PathVariable String arrivalTime) {
-        return scheduleService.calculateFlightDuration(departureTime, arrivalTime);
     }
 }
