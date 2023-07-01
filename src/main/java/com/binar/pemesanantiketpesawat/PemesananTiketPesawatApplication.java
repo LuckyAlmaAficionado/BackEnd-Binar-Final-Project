5a13aaae3e5b2183c5d4364d705bc3cf189c2d72
LuckyAlmaAficionado <luckyrigel9802@gmail.com>
2023-07-01 16:13:20 +0700

    updata notification

--- src/main/java/com/binar/pemesanantiketpesawat/PemesananTiketPesawatApplication.java
+++ src/main/java/com/binar/pemesanantiketpesawat/PemesananTiketPesawatApplication.java
@@ -1,14 +1,37 @@
 package com.binar.pemesanantiketpesawat;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class PemesananTiketPesawatApplication {
 
+    @Value("${app.firebase-configuration-file}")
+    private String firebaseConfiguration;
 
     public static void main(String[] args) {
         SpringApplication.run(PemesananTiketPesawatApplication.class, args);
+    }
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource(firebaseConfiguration).getInputStream());
+
+        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                .setCredentials(googleCredentials).build();
+
+        FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "my-app");
+        return FirebaseMessaging.getInstance(app);
     }
 
 }
